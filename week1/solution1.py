bb8540d88585251d3d28a773961b5b58c4819f7b
IBRAHIM MESTADI <76787094+ibrahim-mestadi@users.noreply.github.com>
2021-11-13 01:32:11 +0100

    solution Of problem1

---
+++ week1/solution1.py
@@ -0,0 +1,10 @@
+# Input input class : 
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# function : 
+def removeDuplicatesFromLinkedList(linkedList):
+    # Write your code here.
+    return None
