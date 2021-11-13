190b56757ec8ea069f580763c8877813ab6e1890
IBRAHIM MESTADI <76787094+ibrahim-mestadi@users.noreply.github.com>
2021-11-14 00:06:53 +0100

    Create solution.cpp

---
+++ week1/problem1(linkedLists/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// input struct. 
+class LinkedList {
+public:
+  int value;
+  LinkedList *next = nullptr;
+
+  LinkedList(int value) { this->value = value; }
+};
+
+
+// solving function 
+LinkedList *removeDuplicatesFromLinkedList(LinkedList *linkedList) {
+  // Write your code here.
+  return nullptr;
+}
